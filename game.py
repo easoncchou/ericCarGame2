659679be0931a5ef5dffe2d71ba5127a0ca342df
Eric Liang <eliangca@outlook.com>
2023-07-05 15:31:03 -0700

    Added Game class

---
+++ game.py
@@ -0,0 +1,24 @@
+class Game:
+    """
+    Game class containing the game loop
+    """
+
+
+    def __init__(self) -> None:
+        """
+        Initializer
+        """
+
+        # does nothing for now...
+
+
+    def run_game_loop(self) -> None:
+        """
+        Runs the game loop
+
+        :return:
+        """
+
+        while True:
+
+            # game stuff
