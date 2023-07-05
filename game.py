b649f5ff38107109df33b06ef8507e923e569ccf
Eason <easonchou2004@gmail.com>
2023-07-05 15:52:08 -0700

    i made stuff like the vairables and the screen fill and the pygaming =)

--- game.py
+++ game.py
@@ -1,15 +1,30 @@
+import pygame
+
 class Game:
     """
     Game class containing the game loop
+
+    === Attributes ===
+    done: whether the game loop is done
     """
 
+    done: bool
 
-    def __init__(self) -> None:
+    def __init__(self, width: int, height: int) -> None:
         """
         Initializer
         """
 
-        # does nothing for now...
+        pygame.init()
+
+        self.done = False
+        self.size = (width, height)
+        self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
+
+        # can set title later
+
+
 
 
     def run_game_loop(self) -> None:
@@ -19,6 +34,10 @@ class Game:
         :return:
         """
 
-        while True:
+        while not self.done:
+            # fill the screen
+            self.screen.fill((255, 255, 255))
 
-            # game stuff
+            # update display
+            pygame.display.flip()
+            self.clock.tick(60)
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from game import *
 
 
 if __name__ == '__main__':
-    game = Game()
+    game = Game(500, 500)
     game.run_game_loop()
