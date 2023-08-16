8c7ff280fc7b9afae5e372ef01bca2b48fb70f84
Eric Liang <eliangca@outlook.com>
2023-08-16 13:36:21 -0700

    Changed error handling on read_message

--- net.py
+++ net.py
@@ -15,15 +15,13 @@ async def read_message(reader) -> bytes:
         raise ConnectionError('Connection closed')
 
     try:
-        int(buf)
+        msg_data = await reader.read(int(buf) + BUFFER_SIZE)
+
+        while len(msg_data) != int(buf):
+            buf = msg_data[int(buf):]
+            msg_data = await reader.read(int(buf) + BUFFER_SIZE)
+
+        return msg_data
     except ValueError:
         await reader.readuntil(separator=b'\n')
         raise Exception('Partial message')
-
-    msg_data = await reader.read(int(buf) + BUFFER_SIZE)
-
-    while len(msg_data) != int(buf):
-        buf = msg_data[int(buf):]
-        msg_data = await reader.read(int(buf) + BUFFER_SIZE)
-
-    return msg_data
